547d9af044a22a0ffa05306b1d0ba68ec04dd88f
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2020-10-06 17:00:00 +0300

    Was fix bugs.

--- Rzproekt/Controllers/RouteController.cs
+++ Rzproekt/Controllers/RouteController.cs
@@ -5,27 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Rzproekt.Models;
 
 namespace Rzproekt.Controllers {
     public class RouteController : Controller {
-        private readonly ILogger<RouteController> _logger;
-
-        public RouteController(ILogger<RouteController> logger) {
-            _logger = logger;
-        }
-
         public IActionResult Index() {
             return View();
-        }
-
-        public IActionResult Privacy() {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
