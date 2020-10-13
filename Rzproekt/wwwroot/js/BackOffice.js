78305e18864cefa9ca4fe4e955473feac34cbcaa
Maldades4689 <36959833+Maldades4689@users.noreply.github.com>
2020-10-13 19:42:29 +0300

    add fix fn onAddCert

--- Rzproekt/wwwroot/js/BackOffice.js
+++ Rzproekt/wwwroot/js/BackOffice.js
@@ -256,10 +256,16 @@ var back_office = new Vue({
             let self = this;
             let sUrl = self.$data.urlApi + '/api/back-office/add-cert';
             let formData = new FormData();
+            let arrCert = [];
 
-            if (!!this.filesCert) formData.set('filesAddCert', this.filesCert);
+            if (!!this.filesCert) {
+                for (let i in this.filesCert) {
+                    if (typeof (this.filesCert[i]) === 'number') break;
+                    arrCert.push(this.filesCert[i]);
+                }
+            }
             try {
-                axios.post(sUrl, formData)
+                axios.post(sUrl, arrCert)
                     .then((response) => {
                         console.log('Данные успешно изменены');
 
