a8423319b339450f25fa56aa000889bb9a0d0f30
Maldades4689 <36959833+Maldades4689@users.noreply.github.com>
2020-10-14 18:34:46 +0300

    fix fn onChangeStat notstringify

--- Rzproekt/wwwroot/js/BackOffice.js
+++ Rzproekt/wwwroot/js/BackOffice.js
@@ -314,7 +314,7 @@ var back_office = new Vue({
                 nThree,
                 sThree
             }
-            oData = JSON.stringify(oData);
+            //oData = JSON.stringify(oData);
             try {
                 axios.post(sUrl, oData)
                     .then((response) => {
