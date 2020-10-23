3396266b175ed6ae928b6d093501607a3ee41aec
Maldades4689 <36959833+Maldades4689@users.noreply.github.com>
2020-10-23 19:47:31 +0300

    fix fn setUserId

--- Rzproekt/wwwroot/js/site.js
+++ Rzproekt/wwwroot/js/site.js
@@ -41,7 +41,7 @@ var appHome = new Vue({
         footer: []
     },
     created() {
-        this.getUserId();
+        //this.getUserId();
     },
         
     // После загрузки страницы вызывает функцию _getData для всех блоков сайта
@@ -335,20 +335,17 @@ var appHome = new Vue({
                 .build();
             let connectionId = "";
             let MessageText = document.getElementById('msgChat').value;
-            let sUrl = appHome.$data.urlApi + "/api/message/send?product=".concat(MessageText);
+            let sUrl = appHome.$data.urlApi + "/api/message/send";
             document.getElementById("sendBtn")
                 .addEventListener("click", function (e) {
                     e.preventDefault();
 
-                    let data = new FormData();
-                    data.set("product", document.getElementById("productField").value);
-                    data.set("connectionId", connectionId);
                     let oData = {
                         UserCode,
                         MessageText
                     }
                     try {
-                        axios.get(sUrl, oData)
+                        axios.post(sUrl, oData)
                             .then((response) => {
                                 console.log("ok");
 
@@ -365,16 +362,16 @@ var appHome = new Vue({
             hubConnection.on("Notify", function (message) {
 
                 // создает элемент <p> для сообщения пользователя
-                let elem = document.createElement("p");
-                elem.appendChild(document.createTextNode(message));
+                //let elem = document.createElement("p");
+                //elem.appendChild(document.createTextNode(message));
 
-                document.getElementById("notify").appendChild(elem);
+                //document.getElementById("notify").appendChild(elem);
 
             });
             hubConnection.start().then(() => {
                 // после соединения получаем id подключения
-                console.log(hubConnection.connectionId);
-                connectionId = hubConnection.connectionId;
+                //console.log(hubConnection.connectionId);
+                //connectionId = hubConnection.connectionId;
             });
         },
         getUserId() {
