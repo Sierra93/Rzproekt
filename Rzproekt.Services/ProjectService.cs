648cd82954f7c504e83f52674618721938a994aa
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2021-03-04 19:48:18 +0300

    Was fixed checks for the main page.

--- Rzproekt.Services/ProjectService.cs
+++ Rzproekt.Services/ProjectService.cs
@@ -304,9 +304,9 @@ namespace Rzproekt.Services {
         /// <returns></returns>
         public async override Task ChangeProjectInfo(ProjectDto projectDto) {
             try {
-                bool bMainFlag = await GetCountProjects() < 3;
+                bool bMainFlag = await GetCountProjects() > 3;
 
-                if (bMainFlag)
+                if (!bMainFlag)
                 {
                     throw new ArgumentException();
                 }
@@ -322,7 +322,7 @@ namespace Rzproekt.Services {
 
             catch (ArgumentException ex)
             {
-                throw new ArgumentException($"недопустимо наличие менее 3 главных проектов {ex.Message}");
+                throw new ArgumentException($"недопустимо наличие более 3 и менее 3 главных проектов {ex.Message}");
             }
 
             catch (Exception ex) {
